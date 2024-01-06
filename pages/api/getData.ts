2b6f5719658eb15c4f6e4ba02e7856b3ca12b5d6
jelly771001 <jelly771001@gmail.com>
2024-01-06 16:25:48 +0800

    updata apiRouter

--- pages/api/getData.ts
+++ pages/api/getData.ts
@@ -11,15 +11,21 @@ const buildFeedbackPath=()=>{
 }
 //讀取檔案
 const extractFeedback = (filePath) => {
-  const fileData = fs.readFileSync(filePath)
-  const data = JSON.parse(fileData)
-  return data
-}
+  if (!fs.existsSync(filePath)) {
+    return [];
+  }
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+};
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   if (req.method === 'GET') {
-     const filePath = buildFeedbackPath()
-    const data = extractFeedback(filePath)
-    res.status(200).json({ feedback: data })
+    try {
+      const filePath = buildFeedbackPath();
+      const data = extractFeedback(filePath);
+      res.status(200).json({ feedback: data });
+    } catch (error) {
+      res.status(500).json({ error: "Something went wrong" });
+    }
   } 
-   
 }
